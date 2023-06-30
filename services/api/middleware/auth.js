ef3fae1fd2b3b121ff309c365a82e45f898e3dca
Zak Fixler <zakfixler@gmail.com>
2023-06-30 07:14:41 -0400

    Fix auth middleware bug

--- services/api/middleware/auth.js
+++ services/api/middleware/auth.js
@@ -2,7 +2,6 @@ export function auth(req, res, next) {
     if (req.session.user) {
         next();
     } else {
-        res.status(403).send('User is not authenticated.')
+        return res.status(403).send('User is not authenticated.')
     }
-	return next();
 }
